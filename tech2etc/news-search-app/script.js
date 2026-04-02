682588b1dbc5d2a0f5ed387114944925600f9b54
hasibul <hasibulhossain157@gmail.com>
2026-04-02 16:38:48 +0600

    add news search app api project

---
+++ tech2etc/news-search-app/script.js
@@ -0,0 +1,97 @@
+const apikey = 'fdcc5c89ac6b4a4891e6f2bab9c90ccf';
+
+const blogContainer = document.getElementById('bolg-container');
+const searchField = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+async function fetchRandomNews(){
+    try {
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+        
+    } catch (error) {
+        console.error('Error fetching random news', error)
+        return[]
+    }
+}
+
+searchButton.addEventListener('click', async ()=> {
+    const query = searchField.value.trim();
+    if(query !== ''){
+        try {
+            const articles = await fetchNewsQuery(query);
+            displayBlogs(articles)
+        } catch (error) {
+            console.log('error fetching news by query', error)
+        }
+    }
+})
+
+
+async function fetchNewsQuery(query){
+    try {
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apikey=${apikey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+        
+    } catch (error) {
+        console.error('Error fetching random news', error)
+        return[]
+    }
+}
+
+
+function displayBlogs(articles){
+    blogContainer.innerHTML = '';
+    articles.forEach((article) => {
+        const blogCard = document.createElement('div');
+        blogCard.classList.add('blog-card');
+        const img = document.createElement('img');
+        img.src = article.urlToImage;
+        img.alt = article.title;
+        const title = document.createElement('h2');
+        
+        const truncatedTitle = article.title && article.title.length > 30 ? article.title.slice(0, 30) + '...' : (article.title || 'No title available');
+        title.textContent = truncatedTitle;
+        const description = document.createElement('p');
+        
+
+        const truncatedDes = article.description && article.description.length > 120 ? article.description.slice(0, 120) + '...' : (article.description || 'No description available');
+
+        description.textContent = truncatedDes;
+
+        blogCard.appendChild(img)
+        blogCard.appendChild(title)
+        blogCard.appendChild(description);
+        blogCard.addEventListener('click', ()=> {
+            window.open(article.url, '_blank');
+        })
+        blogContainer.appendChild(blogCard);
+    })
+}
+
+
+(async ()=> {
+    try {
+       const articles = await fetchRandomNews();
+       displayBlogs(articles)
+    } catch (error) {
+        console.error('Error fetching random news', error)
+        
+    }
+})();
+
+
+
+
+
+
+
+
+
+
+
+
